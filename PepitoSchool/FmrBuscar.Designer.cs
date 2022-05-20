471dffed95a54b95dd174b1fcda5e53104e43837
cesar11b <katanavivas@gmail.com>
2022-05-20 00:57:08 -0600

    cambio

--- PepitoSchool/FmrBuscar.Designer.cs
+++ PepitoSchool/FmrBuscar.Designer.cs
@@ -30,7 +30,7 @@ namespace PepitoSchool
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtbuscarnombre = new System.Windows.Forms.TextBox();
+            this.txtbuscarid = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -39,16 +39,16 @@ namespace PepitoSchool
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(82, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.Size = new System.Drawing.Size(76, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Buscar por nombre";
+            this.label1.Text = "Buscar por Id";
             // 
-            // txtbuscarnombre
+            // txtbuscarid
             // 
-            this.txtbuscarnombre.Location = new System.Drawing.Point(43, 101);
-            this.txtbuscarnombre.Name = "txtbuscarnombre";
-            this.txtbuscarnombre.Size = new System.Drawing.Size(173, 23);
-            this.txtbuscarnombre.TabIndex = 1;
+            this.txtbuscarid.Location = new System.Drawing.Point(43, 101);
+            this.txtbuscarid.Name = "txtbuscarid";
+            this.txtbuscarid.Size = new System.Drawing.Size(173, 23);
+            this.txtbuscarid.TabIndex = 1;
             // 
             // btnBuscar
             // 
@@ -65,7 +65,7 @@ namespace PepitoSchool
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(261, 310);
             this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.txtbuscarnombre);
+            this.Controls.Add(this.txtbuscarid);
             this.Controls.Add(this.label1);
             this.Name = "FmrBuscar";
             this.Text = "FmrBuscar";
@@ -77,7 +77,7 @@ namespace PepitoSchool
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtbuscarnombre;
+        private System.Windows.Forms.TextBox txtbuscarid;
         private System.Windows.Forms.Button btnBuscar;
     }
 }
